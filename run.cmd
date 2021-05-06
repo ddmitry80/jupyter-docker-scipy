0a9f27efe5728118313d650893b71c7efa961f7a
Dmitry Dementiev <ddmitry@gmail.com>
2021-05-06 11:18:11 +0300

    adapt to run service on windows

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
 @rem Запуск контейнера
-docker run --rm -d --name=jupyter-scipy -p 8898:8888 -e JUPYTER_ENABLE_LAB=yes -v /c/ddmitry/YandexDisk/Учеба:/home/jovyan/work/Yandex  -v /c/ddmitry/DataScience:/home/jovyan/work/DataScience -v /c/ddmitry/.ssh:/home/jovyan/.ssh jupyter-docker-scipy start.sh jupyter lab --NotebookApp.password='sha1:eadc47330625:65d799b65d21048a4f96c12d862514466324f0e3'
+docker run --rm -d --name=jupyter-scipy-tmp -p 8893:8888 -e JUPYTER_ENABLE_LAB=yes -v /c/ddmitry/YandexDisk/Учеба:/home/jovyan/work/Yandex  -v /c/ddmitry/Sources:/home/jovyan/work/Sources -v /c/ddmitry/.ssh:/home/jovyan/.ssh jupyter-docker-scipy start.sh jupyter lab --NotebookApp.password='sha1:eadc47330625:65d799b65d21048a4f96c12d862514466324f0e3'
