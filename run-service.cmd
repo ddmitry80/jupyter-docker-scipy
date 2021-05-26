63c27462557a7b58cb30b7a651d1f15b5ae1807b
Dmitry Dementiev <ddmitry@gmail.com>
2021-05-27 00:17:31 +0300

    Use non-system ssh keys and container autorun

--- run-service.cmd
+++ run-service.cmd
@@ -1,2 +1,8 @@
 @rem Запуск контейнера
-docker run -d --name=jupyter-scipy -p 8898:8888 -e JUPYTER_ENABLE_LAB=yes -v /c/ddmitry/YandexDisk/Учеба:/home/jovyan/work/Yandex  -v /c/ddmitry/Sources:/home/jovyan/work/Sources -v /c/ddmitry/.ssh:/home/jovyan/.ssh jupyter-docker-scipy start.sh jupyter lab --NotebookApp.password='sha1:eadc47330625:65d799b65d21048a4f96c12d862514466324f0e3'
+docker run -d --name=jupyter-scipy ^
+-p 8898:8888 -e JUPYTER_ENABLE_LAB=yes ^
+--restart=unless-stopped ^
+-v /c/ddmitry/YandexDisk/Учеба:/home/jovyan/work/Yandex  ^
+-v /c/ddmitry/Sources:/home/jovyan/work/Sources ^
+-v ssh-keys:/home/jovyan/.ssh ^
+jupyter-docker-scipy start.sh jupyter lab --NotebookApp.password='sha1:eadc47330625:65d799b65d21048a4f96c12d862514466324f0e3'
